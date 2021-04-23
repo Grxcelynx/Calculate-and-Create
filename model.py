37946b154ab1c1a57cabad82c3fff4961b20b4a6
Grace <grace.mbirnam@gmail.com>
2021-04-23 23:20:21 +0000

    helo

---
+++ model.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy 
+
+def thisFunc():
+    return thisFunc
+
+def anotherFunc():
+    return anotherFunc
+
+def comeOn():
+    return "dead"
+
+def imdying():
+    return xjkssfhlaw
