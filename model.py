dbcd896360c73fbc959020834c416c9a0048d059
Grace <grace.mbirnam@gmail.com>
2021-04-29 03:52:35 +0000

    finished model.py for now

--- model.py
+++ model.py
@@ -3,12 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 # model.py 
 # test psql like movie ratings
-# Make reltionships to other tables 
-#figure out f strings 
+# Make reltionships to other tables - DONE
+#figure out f strings - DONE 
 
 
 
-class Creation(db.Model):
+class CreationForm(db.Model):
     """Creation form - How a user will start their project"""
 
     __tablename__ = "creation_form"
@@ -21,14 +21,22 @@ class Creation(db.Model):
                             db.ForeignKey('weather.weather_id'))
     paint_id = db.Column(db.Integer,
                             db.ForeignKey('paint_type.paint_id'))
+    
+    canvas_size = db.relationship("CanvasSize", back_populates="creation_form")
+    weather = db.relationship("CanvasSize", back_populates="creation_form")
+    paint_type = db.relationship("CanvasSize", back_populates="creation_form")
+
+    dry_time = db.relationship('DryTime', uselist=False, back_populates='creation_form_id')
+
+
 
     # creation_form = a list of creation objects 
 
     def __repr__(self):
-        return f'<Creation creation_form_id ={self.creation_form_id} email={self.email}>'  
+        return f'<CreationForm creation_form_id ={self.creation_form_id} painting_name={self.painting_name}>'  
 
 
-class Canvas(db.Model):
+class CanvasSize(db.Model):
     """picking the size of canvas"""
 
     __tablename__ = "canvas_size"
@@ -37,12 +45,15 @@ class Canvas(db.Model):
     size = db.Column(db.Integer)
     canvas_time = db.Column(db.Integer)
 
-    creation_form = db.relationship('Creation')
+    creation_form = db.relationship('CreationForm', uselist=False, back_populates='canvas_size')
+
+    # child = relationship("Child", uselist=False, back_populates="parent")
 
     # canvas = options for user to select canvas type that will add to dry time
 
     def __repr__(self):
-        return f'<Creation creation_form_id ={self.creation_form_id} email={self.email}>'
+        return f'<CanvasSize canvas_id ={self.canvas_id} size={self.size}>'
+
 
 class Weather(db.Model):
     """selecting their weather/climate that will affect the dry time"""
@@ -53,13 +64,14 @@ class Weather(db.Model):
     weather_type = db.Column(db.String)
     weather_time = db.Column(db.Integer)
 
+    creation_form = db.relationship('CreationForm', uselist=False, back_populates='weather')
     # weather = options for the user to select the climate they are in that will determine the dry time
 
     def __repr__(self):
-        return f'<Creation creation_form_id ={self.creation_form_id} email={self.email}>'
+        return f'<Weather weather_id ={self.weather_id} weather_type={self.weather_type}>'
 
 
-class Paint(db.Model):
+class PaintType(db.Model):
     """user can select up to 3 kinds of paint they're using to determine final dry time"""
 
     __tablename__ = "paint_type"
@@ -71,11 +83,14 @@ class Paint(db.Model):
     #will i need unique for paint dry time too?
 
     # paint_type = options of paint the user can select 
+    creation_form = db.relationship('CreationForm', uselist=False, back_populates='paint_type')
 
-     def __repr__(self):
-        return f'<Creation creation_form_id ={self.creation_form_id} email={self.email}>'
 
-class Drying(db.Model):
+    def __repr__(self):
+        return f'<PaintType paint_id ={self.paint_id} type_of_paint={self.type_of_paint}>'
+
+
+class DryTime(db.Model):
     """takes in all factors from creation form and adds together for final dry time"""
 
     __tablename__ = "dry_time"
@@ -86,12 +101,16 @@ class Drying(db.Model):
 
     # dry_time = populating final dry time for the result table
 
+    creation_form = db.relationship("CreationForm", back_populates="dry_time")
+
+    final_result = db.relationship('FinalResult', uselist=False, back_populates='dry_time_id')
+
     def __repr__(self):
-        return f'<Creation creation_form_id ={self.creation_form_id} email={self.email}>'
+        return f'<DryTime dry_time_id ={self.dry_time_id} creation_form_id={self.creation_form_id}>'
 
 
 
-class Results(db.Model):
+class FinalResult(db.Model):
     """produces the final result for user"""
 
     __tablename__ = "final_result"
@@ -102,10 +121,22 @@ class Results(db.Model):
 
 # final_result = user's full calculation 
 
- def __repr__(self):
-        return f'<Creation creation_form_id ={self.creation_form_id} email={self.email}>'
+    dry_time = db.relationship('DryTime', back_populates='final_result')
 
-    
+
+def __repr__(self):
+    return f'<FinalResult final_result_id ={self.final_result_id} dry_time_id={self.dry_time_id}>'
+
+def connect_to_db(flask_app, db_uri='postgresql:///creation_form', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+  
 
 
 if __name__ == '__main__':
