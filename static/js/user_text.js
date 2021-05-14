9e18217210c02f48c7effc474ae5310bf1c951c9
Grace <grace.mbirnam@gmail.com>
2021-05-14 03:36:45 +0000

    assed js

---
+++ static/js/user_text.js
@@ -0,0 +1,9 @@
+
+// EXAMPLE CODE
+// Submits phone number to DB for text updates
+// $('#phone-submit').on('click', () => {
+//     $.post('/users/phone-update/api', {'phone': $('#phone-num').val()}, (response) => {
+//       $('#text-modal').modal('toggle');
+//       alert(response);
+//     });
+//   });
