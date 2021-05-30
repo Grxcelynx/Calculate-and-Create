c29e442ecf42a45ca26ce0cd740e72d1df6fd327
Grace <grace.mbirnam@gmail.com>
2021-05-30 02:37:45 +0000

    fixed jquery kind of

--- static/js/calc_examples.js
+++ static/js/calc_examples.js
@@ -1,11 +1,26 @@
-
-
 $.get("/example", (data) => {
-    $('paint_used').text(`${data.paint_used}`);
-    $('canvas_selected').text(`${data.canvas_selected}`);
-    $('weather_condition').text(`${data.weather_condition}`);
-    $('total_dry_time').text(`${data.total_dry_time}`);
+    $('#paint_used').text(data[0].paint);
+    $('#canvas_selected').text(data[0].canvas);
+    $('#weather_condition').text(data[0].weather);
+    $('#total_dry_time').text(data[0].total);
 
-    console.log(data)
+    $('#paint_used').text(data[1].paint);
+    $('#canvas_selected').text(data[1].canvas);
+    $('#weather_condition').text(data[1].weather);
+    $('#total_dry_time').text(data[1].total);
 
+    $('#paint_used').text(data[2].paint);
+    $('#canvas_selected').text(data[2].canvas);
+    $('#weather_condition').text(data[2].weather);
+    $('#total_dry_time').text(data[2].total);
+
+    $('#paint_used').text(data[3].paint);
+    $('#canvas_selected').text(data[3].canvas);
+    $('#weather_condition').text(data[3].weather);
+    $('#total_dry_time').text(data[3].total);
+
+    $('#paint_used').text(data[4].paint);
+    $('#canvas_selected').text(data[4].canvas);
+    $('#weather_condition').text(data[4].weather);
+    $('#total_dry_time').text(data[4].total);
 });
